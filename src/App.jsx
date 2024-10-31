92b7ed620cd8df5020f2cbbc0da2882dc408a470
sreyesg <santiagoreyes.g@gmail.com>
2024-10-30 20:19:12 -0700

    create from for FirstName and declare controlled input elements.

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,22 @@ const App = () => {
   // add placeholder text for the title
   // we'll use the form to update this state by the end of this lesson
   const [title, setTitle] = useState('The full name will appear here.');
-
+  const [firstName, setFirstName] = useState('')
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value)
+    
+  }
   return (
     <>
       <h2>{title}</h2>
       <form>
         <label htmlFor="firstName">First Name: </label>
-        <input id="firstName" name="firstName" />
+        <input 
+          id="firstName" 
+          name="firstName" 
+          value={firstName}
+          onChange={handleFirstNameChange}
+          />
       </form>
     </>
   );
