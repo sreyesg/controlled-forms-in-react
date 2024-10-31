f2238d35a55421dd573e2b5ff48c60994d4b4287
sreyesg <santiagoreyes.g@gmail.com>
2024-10-30 20:01:23 -0700

    Define a controlled input: state, handleChange function, and value tied to state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 // src/App.jsx
 
 import './App.css'
+import { useState } from 'react';
 
 const App = () => {
+
+  const [cityInput, setCityInput] = useState('')
+
+  const handleChange =(event) => {
+    setCityInput(event.target.value)
+  }
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Enter the City</h1>
+      <label htmlFor="cityInput">City: </label>
+      <input 
+        type="text" 
+        name="cityInput" 
+        id="cityInput"
+        value={cityInput}
+        onChange={handleChange}
+      />
+    </>
   );
 };
 
